eeff5b0a10173876b1239a20e5131c0fb1da27bb
Meet Joshi <meetjoshi2018@gmail.com>
2026-03-12 12:10:53 +0530

    initialised backend file

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,23 @@
-from dotenv import load_dotenv
+from agents.stock_agent import stock_agent
 
-load_dotenv()
+
+def main():
+
+    print("Financial Stock Analyst")
+    print("-----------------------")
+
+    while True:
+
+        query = input("\nAsk a question: ")
+
+        if query.lower() in ["exit", "quit"]:
+            break
+
+        response = stock_agent.run(query)
+
+        print("\nAI Analysis:\n")
+        print(response)
+
+
+if __name__ == "__main__":
+    main()
