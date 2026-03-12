e5d3e6f235d95c818ca973c3969e5977ca7ecd56
Meet Joshi <meetjoshi2018@gmail.com>
2026-03-12 12:21:33 +0530

    model changed

--- backend/agents/stock_agent.py
+++ backend/agents/stock_agent.py
@@ -1,11 +1,14 @@
 #this agent will help to know WHY STOCK MOVED
 
+from agno.models.groq import Groq
 from agno.agent import Agent
 from tools.market_tool import get_stock_data
 from tools.news_tool import search_news
-
+from dotenv import load_dotenv
+load_dotenv()
 
 stock_agent = Agent(
+    model=Groq(id="openai/gpt-oss-120b"),
     name="Stock Movement Analyst",
 
     instructions="""
