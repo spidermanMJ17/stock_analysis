d0bb26494ebbc45fd7f71a7ddf527de39ac2fd17
Meet Joshi <meetjoshi2018@gmail.com>
2026-03-13 09:26:01 +0530

    sentiment agent created

---
+++ backend/agents/sentiment_agent.py
@@ -0,0 +1,26 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+
+sentiment_agent = Agent(
+    name="Financial News Sentiment Analyst",
+
+    role="""
+    You are a financial sentiment analyst.
+    Your task is to analyze financial news headlines related to a stock
+    and determine the overall sentiment.
+    """,
+
+    model=Groq(id="openai/gpt-oss-120b"),
+
+    instructions=[
+        "Read the financial news headlines carefully.",
+        "Classify each headline as Bullish, Neutral, or Bearish.",
+        "Count how many headlines fall into each category.",
+        "Compute a sentiment score between -1 and +1.",
+        "Return structured JSON only."
+    ],
+
+    markdown=True
+)
